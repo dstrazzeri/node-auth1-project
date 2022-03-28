1b09f3ebaaac14dbbc3ff279230f3d7e5f56f27e
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-03-28 07:55:23 -0400

    all but one test passing

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -8,13 +8,13 @@ const Users = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
-  if (req.session.user) {
-    next()
-  } else {
-    next(res.status(401).json({
-      message: "You shall not pass!"
-    }))
-  }
+
+    if (req.session.user) {
+
+        next()
+    } else {
+        next(res.status(401).json({message: "You shall not pass!"}))
+    }
 }
 
 /*
@@ -25,22 +25,17 @@ function restricted(req, res, next) {
     "message": "Username taken"
   }
 */
-function checkUsernameFree(req, res, next) {
-  try {
-    const users = await Users.findBy({
-      username: req.body.username
-    })
-    if (!users.length) {
-      next()
-    } else {
-      next({
-        status: 422, 
-        message: 'Username taken'
-      })
+async function checkUsernameFree(req, res, next) {
+    try{
+        const users = await Users.findBy({username: req.body.username})
+        if (!users.length){
+            next()
+        }else {
+            next({ status: 422, message: 'Username taken'})
+        }
+    }catch (err){
+        next(err)
     }
-  } catch(err) {
-    next(err)
-  }
 }
 
 /*
@@ -52,23 +47,17 @@ function checkUsernameFree(req, res, next) {
   }
 */
 async function checkUsernameExists(req ,res, next) {
-  try{
-      const users = await Users.findBy({
-        username: req.body.username
-      })
-      if (users.length){
-          next()
-      }else{
-          next({ 
-            status: 401, 
-            message: 'Invalid credentials'
-          })
-      }
-  }catch (err) {
-      next(err)
-  }
+    try{
+        const users = await Users.findBy({username: req.body.username})
+        if (users.length){
+            next()
+        }else{
+            next({ status: 401, message: 'Invalid credentials'})
+        }
+    }catch (err) {
+        next(err)
+    }
 }
-
 
 /*
   If password is missing from req.body, or if it's 3 chars or shorter
@@ -79,20 +68,18 @@ async function checkUsernameExists(req ,res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  const pass = req.body.password
-  if (!pass || pass.length <=3) {
-    next(res.status(422).json({
-      message: "Password must be longer than 3 chars"
-    }))
-  } else {
-    next()
-  }
+    const pass = req.body.password
+    if (!pass || pass.length <= 3){
+        next(res.status(422).json({ message : "Password must be longer than 3 chars"}))
+    }else {
+        next()
+    }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
-  checkUsernameFree,
-  restricted,
-  checkUsernameExists,
-  checkPasswordLength
+    checkUsernameFree,
+    restricted,
+    checkUsernameExists,
+    checkPasswordLength
 }
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,63 +1,118 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
+const express = require('express')
+const bcrypt = require('bcryptjs')
+const router = express.Router()
+const Users = require('../users/users-model')
+const{
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
+} = require('./auth-middleware')
+
 
 
 /**
-  1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
+ 1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
 
-  response:
-  status 200
-  {
+ response:
+ status 200
+ {
     "user_id": 2,
     "username": "sue"
   }
 
-  response on username taken:
-  status 422
-  {
+ response on username taken:
+ status 422
+ {
     "message": "Username taken"
   }
 
-  response on password three chars or less:
-  status 422
-  {
+ response on password three chars or less:
+ status 422
+ {
     "message": "Password must be longer than 3 chars"
   }
  */
 
-
+router.post('/register',checkUsernameFree,checkPasswordLength,  async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const newUser = {
+      username,
+      password: bcrypt.hashSync(password, 10),
+    }
+    const created = await Users.add(newUser)
+    res.status(201).json({username: created.username, user_id: created.user_id})
+  } catch (err) {
+    next(err)
+  }
+})
 /**
-  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
+ 2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
-  response:
-  status 200
-  {
+ response:
+ status 200
+ {
     "message": "Welcome sue!"
   }
 
-  response on invalid credentials:
-  status 401
-  {
+ response on invalid credentials:
+ status 401
+ {
     "message": "Invalid credentials"
   }
  */
 
+router.post('/login', checkUsernameExists, async (req, res, next) => {
+  try {
 
+    const { username, password } = req.body
+    const [userFromDb] = await Users.findBy({ username })
+
+    const verifies = bcrypt.compareSync(password, userFromDb.password)
+    if (verifies === false) {
+      return res.status(401).json({message: 'Invalid credentials'})
+    }else
+      req.session.user = userFromDb
+    res.status(200).json({message: `Welcome ${username}`})
+  } catch (err) {
+    next(err)
+  }
+})
 /**
-  3 [GET] /api/auth/logout
+ 3 [GET] /api/auth/logout
 
-  response for logged-in users:
-  status 200
-  {
+ response for logged-in users:
+ status 200
+ {
     "message": "logged out"
   }
 
-  response for not-logged-in users:
-  status 200
-  {
+ response for not-logged-in users:
+ status 200
+ {
     "message": "no session"
   }
  */
 
- 
+router.get('/logout', async (req, res, next) => {
+  try {
+    if (req.session.user) {
+      req.session.destroy((err) => {
+        if (err) {
+          res.status(500).json('unknown error')
+        } else {
+          res.status(200).json({message:"logged out"})
+        }
+      })
+    } else {
+      res.status(200).json({message:"no session"})
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router
